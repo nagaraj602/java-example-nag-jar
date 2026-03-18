26b07b03b53663dfee7382e74d6ba04e3d97333b
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-03-18 21:57:46 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        try {
+            String publicIp = new BufferedReader(
+                    new InputStreamReader(new URL("http://checkip.amazonaws.com").openStream())
+            ).readLine();
+
+            System.out.println("\n🔥 You can access the application with: http://" + publicIp + ":8085\n");
+
+        } catch (Exception e) {
+            System.out.println("Unable to fetch public IP");
+        }
     }
 
     @GetMapping("/")
@@ -18,7 +33,7 @@ public class DemoApplication {
         return """
             <html>
                 <body style="margin:0; background-color:#0a2540; display:flex; justify-content:center; align-items:center; height:100vh;">
-                    <h1 style="color:white; font-family:Arial;">
+                    <h1 style="color:white;">
                         Hello, Spring Boot is running on port 8085!
                     </h1>
                 </body>
